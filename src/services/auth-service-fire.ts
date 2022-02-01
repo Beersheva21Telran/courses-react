6133da742fc74ad59c16cbacb9091a54928a4151
Yuragranovsky <yuragranovsky@gmail.com>
2022-02-01 21:06:18 +0200

    final authorization

--- src/services/auth-service-fire.ts
+++ src/services/auth-service-fire.ts
@@ -1,33 +1,45 @@
 import AuthService from "./auth-service";
 import {getAuth, signInWithEmailAndPassword, signOut} from 'firebase/auth';
 import {authState} from 'rxfire/auth';
-import {Observable} from 'rxjs';
+import {from, Observable} from 'rxjs';
 import {map, mergeMap} from 'rxjs/operators';
 import { LoginData } from "../models/common/login-data";
 import { nonAuthorizedUser, UserData } from "../models/common/user-data";
 import { collectionData } from "rxfire/firestore";
 import appFire from "../config/fire-config";
-import { collection, CollectionReference, getFirestore } from "firebase/firestore";
+import { collection, CollectionReference, doc, DocumentReference, DocumentSnapshot, getDoc, getFirestore } from "firebase/firestore";
 export default class AuthServiceFire implements AuthService {
     private authFire = getAuth(appFire);
     private collectionAuth: CollectionReference;
     constructor(private collectionAdministrators: string) {
         this.collectionAuth = collection(getFirestore(appFire), this.collectionAdministrators);
     };
+    async isAdmin(id?: string): Promise<boolean> {
+        if (!id) {
+            return false;
+        }
+
+        const docRef: DocumentReference = doc(this.collectionAuth, id);
+        const docSnap: DocumentSnapshot = await getDoc(docRef);
+
+        return docSnap.exists();
+    }
+
     getUserData(): Observable<UserData> {
-        return authState(this.authFire).pipe(
-            mergeMap(userFire => {
-                return collectionData(this.collectionAuth).pipe(
-                    map(admins => (
-                        !!userFire ? {
-                                    username: userFire.uid,
-                                    displayName: userFire.displayName || userFire.email as string,
-                                    isAdmin: admins.findIndex(doc => doc.email == userFire.email) >= 0
-                                } : nonAuthorizedUser
-                    ))
-                )
-            })
-        )
+        return authState(this.authFire)
+            .pipe ( mergeMap(user => from(this.isAdmin(user?.uid))
+                .pipe(map((isAdmin) => {
+                    if (!!user) {
+                        return {
+                            username: user.uid,
+                            displayName: user.displayName ?? user.email!,
+                            isAdmin: isAdmin
+                        };
+                    }
+
+                    return nonAuthorizedUser;
+                }))
+            ));
     }
     login(loginData: LoginData): Promise<boolean> {
         return signInWithEmailAndPassword(this.authFire, loginData.email, loginData.password)
