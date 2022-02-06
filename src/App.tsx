aa52f74c7f956d3ae058f1c9cf4f65a7b4713320
Yuragranovsky <yuragranovsky@gmail.com>
2022-02-06 13:03:17 +0200

    adds applying redux-thunk

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,19 @@
 
-import { FC, ReactNode, useCallback, useEffect, useState } from 'react';
+import { FC, ReactNode, useCallback, useEffect, useState, Fragment } from 'react';
 import { BrowserRouter, Route, Routes, Navigate } from 'react-router-dom';
 import NavigatorResponsive from './components/common/navigator-responsive';
 import { Subscription } from 'rxjs';
 import { developmentRoutes, PATH_LOGOUT, routes } from './config/routes-config';
 import { authService, college } from './config/service-config';
-import CoursesStore from './models/courses-store-type';
-import CoursesContext, { initialCourses } from './store/context';
 import { RouteType } from './models/common/route-type';
 import { DISPLAY_NAME_ERROR, nonAuthorizedUser, UserData } from './models/common/user-data';
 import { Alert } from '@mui/material';
 import process from 'process';
 import ErrorCode from './models/common/error-code';
 import { AUTH_TOKEN } from './services/courses-service-rest';
-import {useDispatch, useSelector} from 'react-redux'
-import { userDataSelector } from './redux/store';
-import { setCourses, setUserData } from './redux/actions';
+import { useDispatch, useSelector } from 'react-redux'
+import { errorCodeSelector, userDataSelector } from './redux/store';
+import { setCourses, setErrorCode, setUserData } from './redux/actions';
 
 function getRelevantRoutes(userData: UserData): RouteType[] {
   let resRoutes = routes;
@@ -29,35 +27,22 @@ function getRelevantRoutes(userData: UserData): RouteType[] {
 }
 
 const App: FC = () => {
-const userData: UserData = useSelector(userDataSelector);
-const dispatch = useDispatch();
+  const userData: UserData = useSelector(userDataSelector);
+  const code: ErrorCode = useSelector(errorCodeSelector);
+  const dispatch = useDispatch();
   const [flErrorServer, setFlErrorServer] = useState<boolean>(false);
-  const functionsInit = useCallback(() => {
-    initialCourses.add = course => college.addCourse(course).catch(err => handleError(err));
-    initialCourses.remove = id => college.removeCourse(id).catch(err => handleError(err));
-    initialCourses.update = (id, newCourse) =>
-      college.updateCourse(id, newCourse).catch(err => handleError(err));
-  }, [])
-  
-  useEffect(() => {
-    functionsInit();
-  }, [functionsInit])
-
+  const handleErrorCallback = useCallback(handleError, [code]);
+ 
 
   const [relevantRoutes, setRelevantRoutes] = useState<RouteType[]>(routes);
-  function handleError(code: ErrorCode) {
-
+  function handleError() {
     if (code === ErrorCode.NO_ERROR) {
-    
       setFlErrorServer(false);
-     
-
     } else if (code === ErrorCode.AUTH_ERROR) {
       if (!!userData.username) {
         authService.logout();
       }
       setFlErrorServer(false)
-
 
     } else {
       setFlErrorServer(true);
@@ -66,6 +51,7 @@ const dispatch = useDispatch();
   function getRoutes(): ReactNode[] {
     return relevantRoutes.map((r: RouteType) => <Route key={r.path} path={r.path} element={r.element} />)
   }
+  useEffect(() => handleErrorCallback(), [handleErrorCallback]);
   useEffect(() => {
     setRelevantRoutes(getRelevantRoutes((userData)));
 
@@ -73,14 +59,14 @@ const dispatch = useDispatch();
   useEffect(() => {
 
     function getUserData(): Subscription {
-     
+
       return authService.getUserData().subscribe({
         next(ud) {
           if (ud.displayName == DISPLAY_NAME_ERROR) {
-            handleError(ErrorCode.SERVER_UNAVAILABLE)
+            dispatch(setErrorCode(ErrorCode.SERVER_UNAVAILABLE));
           } else {
-            handleError(ErrorCode.NO_ERROR);
-           dispatch(setUserData(ud))
+            dispatch(setErrorCode(ErrorCode.NO_ERROR));
+            dispatch(setUserData(ud))
           }
 
         }
@@ -94,20 +80,20 @@ const dispatch = useDispatch();
     return () => subscriptionUserData.unsubscribe();
   }, [])
   useEffect(() => {
-    let subscription:any;
-     subscription = getData();
+    let subscription: any;
+    subscription = getData();
     function getData(): Subscription {
-      
+
       subscription && subscription.unsubscribe();
       return college.getAllCourses().subscribe({
 
         next(arr) {
-          handleError(ErrorCode.NO_ERROR);
-          
+          dispatch(setErrorCode(ErrorCode.NO_ERROR));
+
           dispatch(setCourses(arr))
         },
         error(err) {
-          handleError(err);
+          dispatch(setErrorCode(err));
           setTimeout(() => { subscription = getData() }, 2000);
         }
 
@@ -117,7 +103,7 @@ const dispatch = useDispatch();
 
     return () => subscription.unsubscribe();
   }, [])
-  return <CoursesContext.Provider value={initialCourses}>
+  return <Fragment>
     {flErrorServer ? <Alert severity='error'>Server is unavailable</Alert> :
       <BrowserRouter>
         <NavigatorResponsive items={relevantRoutes} />
@@ -131,7 +117,7 @@ const dispatch = useDispatch();
 
       </BrowserRouter>}
 
-  </CoursesContext.Provider>
+  </Fragment>
 }
 
 export default App;
--- src/components/pages/development/generation.tsx
+++ src/components/pages/development/generation.tsx
@@ -1,20 +1,22 @@
 import {FC, useContext, useEffect, useState} from 'react';
-import CoursesContext from '../../../store/context';
+
 import { TextField, Button, Box } from '@mui/material';
 import { createRandomCourses } from '../../../utils/random-course';
+import { college } from '../../../config/service-config';
+
 const defaultValue = 10;
 const MAX_COURSES = 100;
 const Generation: FC = () => {
     const [nCourses, setNCourses] = useState(defaultValue);
     const [isValid, setValid] = useState<boolean>(true);
-    const storeValue = useContext(CoursesContext);
+    
     function changeHandler(event: any) {
         setNCourses(+event.target.value);
     }
       function generate() {
         const courses = createRandomCourses(nCourses);
         courses.forEach(async (course)=> {
-            await storeValue.add!(course)
+            await college.addCourse(course)
         })
     }
     useEffect(() => {
